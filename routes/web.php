a075b372ef7bb70b80b4e348663dfb4b2b7f7186
F313-001\user <angel9320909@gmail.com>
2024-10-15 12:05:48 +0800

    sdgs-1修正

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/sdgd', function () {
+Route::get('/sdgs', function () {
     return view('intro_sdgs');
 });
