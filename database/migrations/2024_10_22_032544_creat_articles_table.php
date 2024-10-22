5a63b5cf40c4ddbd65700befedd2e30c42afaa5d
F313-001\user <angel9320909@gmail.com>
2024-10-22 12:19:59 +0800

    資料庫修改

---
+++ database/migrations/2024_10_22_032544_creat_articles_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePopulationStatisticsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('population_statistics', function (Blueprint $table) {
+            $table->id();
+            $table->string('region', 100)->comment('地區名稱');
+            $table->integer('household_count_113_1')->nullable()->comment('113-1期的戶數統計總數');
+            $table->integer('household_count_male_113_1')->nullable()->comment('113-1期的戶數統計男性數');
+            $table->integer('household_count_female_113_1')->nullable()->comment('113-1期的戶數統計女性數');
+            $table->integer('population_count_113_1')->nullable()->comment('113-1期的人口總數');
+            $table->integer('population_count_male_113_1')->nullable()->comment('113-1期的人口統計男性數');
+            $table->integer('population_count_female_113_1')->nullable()->comment('113-1期的人口統計女性數');
+            $table->integer('household_count_113_2')->nullable()->comment('113-2期的戶數統計總數');
+            $table->integer('household_count_male_113_2')->nullable()->comment('113-2期的戶數統計男性數');
+            $table->integer('household_count_female_113_2')->nullable()->comment('113-2期的戶數統計女性數');
+            $table->integer('population_count_113_2')->nullable()->comment('113-2期的人口總數');
+            $table->integer('population_count_male_113_2')->nullable()->comment('113-2期的人口統計男性數');
+            $table->integer('population_count_female_113_2')->nullable()->comment('113-2期的人口統計女性數');
+            $table->timestamps(); // 自動創建 created_at 和 updated_at 欄位
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('population_statistics');
+    }
+}
