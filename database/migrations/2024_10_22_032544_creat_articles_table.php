86702a11494b396d10d35e6d61ee87d12ca74a2c
F313-001\user <angel9320909@gmail.com>
2024-11-15 14:44:49 +0800

    no message

--- database/migrations/2024_10_22_032544_creat_articles_table.php
+++ database/migrations/2024_10_22_032544_creat_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePopulationStatisticsTable extends Migration  // 修改類別名稱為 CreatePopulationStatisticsTable
+class CreatArticlesTable extends Migration  
 {
     /**
      * Run the migrations.
